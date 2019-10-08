2b947350551495ab48001c9c48998606f2109c1e
zhangchaojie <1098626505@qq.com>
2019-10-08 22:53:11 +0800

    doc: update docs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/form/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-ele-form-generator/' : '/',
+  outputDir: 'docs'
 }
