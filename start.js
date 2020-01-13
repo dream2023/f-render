50765aa6cfe43925b9a39b28258bb82897153f6c
zhangchaojie <1098626505@qq.com>
2020-01-13 23:49:39 +0800

    chore: 移动依赖 & 修复本地启动bug

--- start.js
+++ start.js
@@ -1,4 +1,30 @@
-const exec = require('shelljs').exec
+const exec = require('child_process').exec
 const argv = process.argv.slice(2)
 const port = argv[1] || '54321'
-exec(`anywhere -d ./dist -p ${port}`)
+const handler = require('serve-handler')
+const http = require('http')
+const path = require('path')
+const spawn = require('child_process').spawn
+
+const openURL = function (url) {
+  switch (process.platform) {
+    case 'darwin':
+      exec('open ' + url)
+      break
+    case 'win32':
+      exec('start ' + url)
+      break
+    default:
+      spawn('xdg-open', [url])
+  }
+}
+const server = http.createServer((request, response) => {
+  return handler(request, response, {
+    public: path.resolve(__dirname, './dist')
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`)
+  openURL(`http://localhost:${port}`)
+})
