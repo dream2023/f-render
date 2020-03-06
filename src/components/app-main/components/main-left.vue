7447009c62637f87e9ae8e9640fbed056470980b
IWANABETHATGUY <974153916@qq.com>
2020-03-06 16:03:52 +0800

    feat: fuzzySearch

--- src/components/app-main/components/main-left.vue
+++ src/components/app-main/components/main-left.vue
@@ -31,6 +31,7 @@ import draggable from "vuedraggable";
 import { addFormItem } from "@/helpers/tool";
 import { createComponent, ref, computed } from "@vue/composition-api";
 import { Comp } from "@/types/comp";
+import { fuzzySearch } from "../../../helpers/utils";
 
 export default createComponent({
   name: "AppMainLeft",
@@ -47,7 +48,9 @@ export default createComponent({
           return comps;
         } else {
           return comps.filter(
-            item => item.type.includes(keyword) || item.label.includes(keyword)
+            item =>
+              fuzzySearch(item.type, keyword) ||
+              fuzzySearch(item.label, keyword)
           );
         }
       });
@@ -57,11 +60,7 @@ export default createComponent({
       };
     };
 
-    const { searchValue, filteredComps } = search();
-    return {
-      searchValue,
-      filteredComps
-    };
+    return search();
   },
   methods: {
     handleAddFormItem({ label, type }: Comp) {
