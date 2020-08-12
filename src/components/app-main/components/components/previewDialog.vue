db602073f5fc93c67b6d31b13a39aa48888c76e9
zhangchaojie <1098626505@qq.com>
2020-08-12 21:12:30 +0800

    fix: 预览清空原数据

--- src/components/app-main/components/components/previewDialog.vue
+++ src/components/app-main/components/components/previewDialog.vue
@@ -5,11 +5,14 @@
     @update:visible="$emit('update:visible', $event)"
     title="预览"
     width="90%"
+    @open="isShowContent = true"
+    @closed="isShowContent = false"
   >
     <ele-form
       :formDesc="computedFormDesc"
       v-model="formData"
       :visible="visible"
+      v-if="isShowContent"
       @update:visible="$emit('update:visible', $event)"
       :request-fn="handleRequest"
       @request-success="handleRequestSuccess"
@@ -39,8 +42,16 @@ export default {
   },
   data() {
     return {
+      isShowContent: false,
       formData: {}
     };
+  },
+  watch: {
+    visible(val) {
+      if (!val) {
+        this.formData = {};
+      }
+    }
   },
   computed: {
     // 需要加一层 clone, 因为 ele-form会修改内部属性
