1f043eec6d86709cedcbed33132abd4e08239051
zhangchaojie <1098626505@qq.com>
2020-08-24 20:24:52 +0800

    feat: 增加 time-picker 配置

--- src/fixtures/comps.js
+++ src/fixtures/comps.js
@@ -1036,6 +1036,68 @@ export default [
     }
   },
   {
+    type: "time-picker",
+    label: "时间Picker",
+    sort: 12,
+    config: {
+      url:
+        "https://element.eleme.cn/#/zh-CN/component/time-picker#ren-yi-shi-jian-dian",
+      attrs: {
+        config: {
+          placeholder: {
+            type: "input",
+            label: "占位内容"
+          },
+          clearable: {
+            type: "switch",
+            label: "是否显示清除按钮"
+          },
+          editable: {
+            type: "switch",
+            label: "文本框可输入"
+          },
+          size: {
+            type: "select",
+            label: "输入框尺寸",
+            options: [{ text: "默认", value: null }, "medium", "small", "mini"]
+          },
+          popperClass: {
+            type: "input",
+            label: "TimePicker 下拉框的类名"
+          },
+          prefixIcon: {
+            type: "input",
+            label: "自定义头部图标的类名"
+          },
+          clearIcon: {
+            type: "input",
+            label: "自定义清空图标的类名"
+          },
+          readonly: {
+            type: "switch",
+            label: "完全只读"
+          }
+        },
+        defaultData: {
+          readonly: false,
+          editable: true,
+          clearable: true,
+          size: null,
+          prefixIcon: "el-icon-time",
+          clearIcon: "el-icon-circle-close"
+        }
+      },
+      common: {
+        config: {
+          default: {
+            type: "time",
+            label: "默认值"
+          }
+        }
+      }
+    }
+  },
+  {
     type: "timerange",
     label: "时间范围",
     sort: 13,
